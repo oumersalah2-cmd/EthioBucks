49746f886d66b7254c41f6e3afa4531e4503666b
Abdusalam Oumer <oumersalah2@gmail.com>
2026-03-31 11:59:20 +0300

    Fix root URL 404 error

--- ethiobucks_backend/urls.py
+++ ethiobucks_backend/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # This connects your tasks app to the main website
     path('tasks/', include('tasks.urls', namespace='tasks')),
     path('accounts/', include('django.contrib.auth.urls')),
+    # Redirect base URL to tasks application
+    path('', RedirectView.as_view(url='/tasks/', permanent=False)),
 ]
 
 # This block MUST be outside the square brackets [ ]
